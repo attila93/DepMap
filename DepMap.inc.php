ab95363439209e344f556a319e09a7f61297fb63
attila93 <pivodaattila@gmail.com>
2018-09-02 11:31:36 +0200

    Update DepMap.inc.php

--- DepMap.inc.php
+++ DepMap.inc.php
@@ -52,7 +52,7 @@ foreach ($devices as $items) {
     if (!array_key_exists($local_device_id, $devices_by_id)) {
         $items['sysName'] = $items['local_sysName'];
 		// Note - Does not create device link within LibreNMS to allow "onclick" events on the graph
-        $devices_by_id[$local_device_id] = array('id'=>$local_device_id,'label'=>shorthost(format_hostname($items, $items['local_hostname']), 1),'title'=>generate_device_link($local_device, '', array(), '', '', '', 0),'shape'=>'box');
+        $devices_by_id[$local_device_id] = array('id'=>$local_device_id,'label'=>shorthost(format_hostname($items, $items['local_hostname']), 1).' '.$items['local_hostname']','title'=>generate_device_link($local_device, '', array(), '', '', '', 0),'shape'=>'box');
     }
 	$remote_device_id = $items['remote_device_id'];
 	if (!array_key_exists($remote_device_id, $devices_by_id)) {
